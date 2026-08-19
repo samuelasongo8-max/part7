f9c48abfdfee7c8b542609041bc542c3fdf30f86
Samuel Asongo <samuelasongo8@gmail.com>
2026-08-19 11:52:15 +0300

    fix 7.1 useField hook

---
+++ 7.1useField hook/src/components/CreateNew.jsx
@@ -0,0 +1,50 @@
+import { useNavigate } from 'react-router-dom'
+import { useField } from '../hooks'
+
+const CreateNew = ({ addAnecdote }) => {
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
+
+  const navigate = useNavigate()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    addAnecdote({
+      content: content.value,
+      author: author.value,
+      info: info.value,
+      votes: 0
+    })
+
+    navigate('/')
+  }
+
+  return (
+    <div>
+      <h2>Create a new anecdote</h2>
+
+      <form onSubmit={handleSubmit}>
+        <div>
+          content
+          <input {...content} />
+        </div>
+
+        <div>
+          author
+          <input {...author} />
+        </div>
+
+        <div>
+          url for more info
+          <input {...info} />
+        </div>
+
+        <button type="submit">create</button>
+      </form>
+    </div>
+  )
+}
+
+export default CreateNew
